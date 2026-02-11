bc226e5b0f21ca88c8d3940385da0c9b2e213b6e
ivanmaulana1541 <ivan.maulana1541@gmail.com>
2026-02-11 20:14:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,7 @@ import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, si
 import { getDatabase, ref, update, push, onValue, remove } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-database.js";
 import { getStorage, ref as sRef, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-storage.js";
 
-const firebaseConfig={
-apiKey:"AIza...",
-authDomain:"closr...",
-databaseURL:"https://...",
-projectId:"...",
-storageBucket:"..."
-};
-
+const firebaseConfig={apiKey:"AIza...",authDomain:"...",databaseURL:"...",projectId:"...",storageBucket:"..."};
 const app=initializeApp(firebaseConfig);
 const auth=getAuth(app);
 const db=getDatabase(app);
@@ -20,6 +13,9 @@ let map=L.map("map").setView([-6.2,106.8],15);
 L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png").addTo(map);
 
 let myUID,myLat,myLng;
+let myFriends={};
+let momentMarkers={};
+
 const loginBox=document.getElementById("loginBox");
 
 function showLogin(){
@@ -49,11 +45,26 @@ map.setView([myLat,myLng],16);
 
 window.centerMe=()=>{if(myLat) map.setView([myLat,myLng],17);};
 
+/////////////////////////////////////////////////
+// FRIEND CACHE
+/////////////////////////////////////////////////
+
+function loadFriends(){
+onValue(ref(db,"friends/"+myUID),snap=>{
+myFriends=snap.val()||{};
+});
+}
+
+/////////////////////////////////////////////////
+// MOMENT POST
+/////////////////////////////////////////////////
+
 window.openMoment=()=>momentBox.style.display="block";
 
 window.sendMoment=async()=>{
 let text=momentText.value.trim();
 if(!text) return;
+
 let photo="";
 let file=momentPhoto.files[0];
 if(file){
@@ -61,12 +72,86 @@ let r=sRef(storage,"moments/"+myUID+"/"+Date.now());
 await uploadBytes(r,file);
 photo=await getDownloadURL(r);
 }
-push(ref(db,"moments/"+myUID),{text,photo,lat:myLat,lng:myLng,time:Date.now()});
+
+push(ref(db,"moments/"+myUID),{
+text,photo,lat:myLat,lng:myLng,
+time:Date.now(),
+friendsOnly:friendOnly.checked
+});
+
 momentBox.style.display="none";
 };
 
 /////////////////////////////////////////////////
-// FRIEND SEARCH
+// VISIBILITY CHECK
+/////////////////////////////////////////////////
+
+function canSee(owner,data){
+if(!data.friendsOnly) return true;
+return owner===myUID || myFriends[owner];
+}
+
+/////////////////////////////////////////////////
+// MAP MOMENTS
+/////////////////////////////////////////////////
+
+onValue(ref(db,"moments"),snap=>{
+
+for(let k in momentMarkers) map.removeLayer(momentMarkers[k]);
+momentMarkers={};
+
+snap.forEach(user=>{
+let owner=user.key;
+
+user.forEach(m=>{
+let d=m.val();
+if(!canSee(owner,d)) return;
+
+let mk=L.marker([d.lat,d.lng])
+.addTo(map)
+.bindPopup(`📍 ${d.text}`);
+momentMarkers[m.key]=mk;
+});
+
+});
+
+});
+
+/////////////////////////////////////////////////
+// TIMELINE
+/////////////////////////////////////////////////
+
+window.toggleTimeline=()=>{
+timelineBox.style.display=
+timelineBox.style.display==="none"?"block":"none";
+};
+
+onValue(ref(db,"moments"),snap=>{
+
+let feed=[];
+
+snap.forEach(user=>{
+let owner=user.key;
+user.forEach(m=>{
+let d=m.val();
+if(canSee(owner,d)) feed.push(d);
+});
+});
+
+feed.sort((a,b)=>b.time-a.time);
+
+let html="<b>Timeline</b><br><br>";
+
+feed.forEach(f=>{
+html+=`📍 ${f.text}<hr>`;
+});
+
+timelineBox.innerHTML=html;
+
+});
+
+/////////////////////////////////////////////////
+// FRIEND SEARCH + REQUEST
 /////////////////////////////////////////////////
 
 window.openFriend=()=>friendBox.style.display="block";
@@ -119,6 +204,7 @@ window.acceptFriend=(uid)=>{
 update(ref(db,"friends/"+myUID),{[uid]:true});
 update(ref(db,"friends/"+uid),{[myUID]:true});
 remove(ref(db,"friendRequests/"+myUID+"/"+uid));
+loadFriends();
 loadRequests();
 };
 
@@ -136,6 +222,7 @@ if(user){
 myUID=user.uid;
 ensureUsername();
 startGPS();
+loadFriends();
 loginBox.innerHTML=`Logged in<br><button onclick="signOut(auth)">Logout</button>`;
 }else showLogin();
 });
